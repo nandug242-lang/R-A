27037c39af2ede3f300984d1ed1777c269a9e376
nandug242-lang <nandug242@gmail.com>
2026-08-30 14:49:56 +0530

    Fix MainActivity for Rythu Aasara

--- MainActivity.java
+++ MainActivity.java
@@ -1,20 +1,24 @@
 package com.ra.rythuaasara;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.webkit.WebView;
 import android.webkit.WebSettings;
-import androidx.appcompat.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         webView = findViewById(R.id.webview);
+
+        webView.setWebViewClient(new WebViewClient());
 
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
